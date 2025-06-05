57e4808aad0cf999b2986c23614fc020212a66f3
PimpMyGit <tommasoredaelli276@gmail.com>
2025-06-05 17:10:21 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# flooded-buildings
-Little procedures to get buildings at risk of flooding from a flood map
+# safer-buildings
+Little procedures to get buildings at risk of flooding from a flood map.
+
+Examples:
+1. `safer-buildings --wd <waterdepth.tif> --provider OVERTURE --filters "[{'subtype':'education', 'class': ['kindergarten','school']}, {'class':'parking'}] --only_flood --stats"`
+2. `safer-buildings --wd <waterdepth.tif> --provider REGIONE-EMILIA-ROMAGNA-30 --filters "[{'ORDINE_NORMALIZZATO': ['Scuola primaria', 'Nido d\'infanzia']}, {'ISTITUZIONE_SCOLASTICA_RIF': 'IC ALIGHIERI'}]"`
+
+In first example the water depth file is 'tests/rimini-wd.tif', the OVERTURE provider is used, and buildings are filtered is (subtype in ['education'] AND class in ['kindergarten', 'school']) OR (class in ['parking']).
--- src/flooded_buildings/main.py
+++ src/flooded_buildings/main.py
@@ -554,7 +554,7 @@ def main(wd, buildings, wd_thresh, bbox, out, t_srs, provider, filters, only_flo
     Main function to run the flooded buildings analysis from command line.
     
     Examples:
-    1. safer-buildings --wd tests\rimini-wd.tif --provider OVERTURE --filters "[{'subtype':'education', 'class': ['kindergarten','school']}, {'class':'parking'}]"
+    1. safer-buildings --wd tests\rimini-wd.tif --provider OVERTURE --filters "[{'subtype':'education', 'class': ['kindergarten','school']}, {'class':'parking'}] --only_flood --stats"
     2. safer-buildings --wd tests\rimini-wd.tif --provider REGIONE-EMILIA-ROMAGNA-30 --filters "[{'ORDINE_NORMALIZZATO': ['Scuola primaria', 'Nido d\'infanzia']}, {'ISTITUZIONE_SCOLASTICA_RIF': 'IC ALIGHIERI'}]"
     
     In first example the water depth file is 'tests/rimini-wd.tif', the OVERTURE provider is used, and buildings are filtered is (subtype in ['education'] AND class in ['kindergarten', 'school']) OR (class in ['parking']).
