e2fa03b0663a88252e5a4f5058d9bc8220508bda
PimpMyGit <tommasoredaelli276@gmail.com>
2025-06-24 09:47:17 +0200

    add example lambda input status

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,6 +2,16 @@ from safer_buildings import parse_event
 from safer_buildings import compute_flood as main_function
 
 
+# DOC: An example test input for directed project area:
+# {
+#   "water": "s3://saferplaces.co/Directed/data-fabric-rwl2/WD_radar_saferplaces_edekmzo.tif",
+#   "building": "s3://saferplaces.co/Directed/process_out/SaferBuildingsService/Data/buildings-default-area__rer-rest_overture.geojson",
+#   "out": "s3://saferplaces.co/Directed/process_out/SaferBuildingsService/test-rimini-wd-00-output-00.geojson",
+#   "provider": "RER-REST/1",
+#   "summary": true
+# }
+
+
 def lambda_handler(event, context):
     """
     lambda_handler - lambda function
